fd7f7acd85e54e5f0059dcfcf6172acc75275cd3
MfalmeWaWafu <shishkov_alone@mail.ru>
2018-02-05 13:41:23 +0400

    Доделки на занятии(Класс, тест на разность)

--- ConsoleCalc/ConsoleCalc/Program.cs
+++ ConsoleCalc/ConsoleCalc/Program.cs
@@ -13,6 +13,19 @@ namespace ConsoleCalc
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Калькулятор");
             Console.ResetColor();
+            var operation = args[0];
+            if (operation == "sum")
+            {
+                var number1 = Convert.ToInt32(args[1]);
+                var number2 = Convert.ToInt32(args[2]);
+                var res = number1 + number2;
+                //Console.WriteLine($"sum(" + number1 + "," + number2 + ")=" + res);
+                Console.WriteLine($"sum({number1},{number2}) = {res}");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
             Console.ReadKey();
         }
     }
