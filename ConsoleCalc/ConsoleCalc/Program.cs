b76aee9f0e7809097d0762531db0ec4deea82d3a
MfalmeWaWafu <shishkov_alone@mail.ru>
2018-02-06 10:33:33 +0400

    Удалил комменченный код

--- ConsoleCalc/ConsoleCalc/Program.cs
+++ ConsoleCalc/ConsoleCalc/Program.cs
@@ -53,23 +53,6 @@ namespace ConsoleCalc
                 Console.WriteLine("Неверные аргументы");
             }
             Console.ReadKey();
-            //Console.ForegroundColor = ConsoleColor.Green;
-            //Console.WriteLine("Калькулятор");
-            //Console.ResetColor();
-            //var operation = args[0];
-            //if (operation == "sum")
-            //{
-            //    var number1 = Convert.ToInt32(args[1]);
-            //    var number2 = Convert.ToInt32(args[2]);
-            //    var res = number1 + number2;
-            //    //Console.WriteLine($"sum(" + number1 + "," + number2 + ")=" + res);
-            //    Console.WriteLine($"sum({number1},{number2}) = {res}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Error");
-            //}
-            //Console.ReadKey();
         }
     }
 }
